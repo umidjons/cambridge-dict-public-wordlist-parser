4d4b04bc6b9a35c0f027cfa0d3ce72fdf53f5ca8
umidjons <almatov.us@gmail.com>
2020-02-23 14:46:12 +0500

    Bug fixed - doesn't include the same word with other definition

--- ui/src/app/services/quiz.service.ts
+++ ui/src/app/services/quiz.service.ts
@@ -18,29 +18,40 @@ export class QuizService {
     this.words = await this.http.post<IWord[]>(`${env.apiUrl}/word-list`, {url}).toPromise();
   }
 
-  generate(quantity: number = 5) {
+  generate(quantity: number = 50): IQuiz[] {
     const result = [];
 
-    for (let i = 0; i < quantity; i++) {
+    const qty = Math.min(quantity, this.words.length);
 
-      const words = _.shuffle(this.words);
-      const chosen = words[0];
-
-      result.push({
-        question: chosen.word,
-        answers: _.shuffle(words.slice(0, 4)
-          .map(answer => {
-            return {
-              text: answer.definition,
-              correct: chosen.word === answer.word
-            };
-          })),
-        showed: 0
-      });
-
+    for (let i = 0; i < qty; i++) {
+      result.push(this.generateQuiz())
     }
 
     return result;
+  }
+
+  generateQuiz(answerQty?: number): IQuiz {
+    const words = _.shuffle(this.words);
+    const chosen = words[0];
+
+    return {
+      question: chosen.word,
+      answers: this.generateAnswers(chosen, words, answerQty),
+      showed: 0
+    };
+  }
+
+  generateAnswers(chosenWord: IWord, words: IWord[], quantity = 4): IAnswer[] {
+    const filtered = words
+      .filter(word => chosenWord.word !== word.word)
+      .slice(0, Math.min(quantity, words.length))
+      .concat([chosenWord]);
+
+    return _.shuffle(filtered)
+      .map(answer => ({
+        text: answer.definition,
+        correct: chosenWord.word === answer.word
+      }));
   }
 
   check(test: IQuiz, chosen: IAnswer): void {
